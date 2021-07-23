10b8cda98b24205de77a97fbaffe43a6666f208c
Jordi <jordi@air-de-jordi.home>
2021-07-23 15:13:11 +0200

    Camera to save picture

--- app/src/main/java/com/example/beaconinyourcar/MainActivity.kt
+++ app/src/main/java/com/example/beaconinyourcar/MainActivity.kt
@@ -1,22 +1,92 @@
 package com.example.beaconinyourcar
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import java.io.File
+import java.io.FileOutputStream
 
 
 class MainActivity : AppCompatActivity() {
-    val RESP_TOMAR_FOTO = 1000
-    var mImageView = null
+
+    var thumBnail: Bitmap ?= null
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+        var btn_save:ImageView = findViewById<ImageView>(R.id.btn_save)
+        btn_save.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_REQUEST_CODE){
+            if(grantResults.isNotEmpty()&&grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, CAMERA_REQUEST_CODE)
+            }else{
+                Toast.makeText(this, "The aplication needs camera permission.", Toast.LENGTH_LONG)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode== CAMERA_REQUEST_CODE){
+                thumBnail = data!!.extras!!.get("data") as Bitmap
+                Toast.makeText(this, "The pick was saved.", Toast.LENGTH_LONG).show()
+                //var imageView: ImageView ?= null
+                // imageView!!.setImageBitmap(thumBnail)
+                // saveImage(thumBnail!!)
+            }
+        }
+    }
+
+    fun saveImage(bmp: Bitmap) {
+        val file_path = Environment.getExternalStorageDirectory().absolutePath +
+                "/CarImage"
+        val dir = File(file_path)
+        if (!dir.exists()) dir.mkdirs()
+        val file = File(dir, "car" + ".png")
+        val fOut = FileOutputStream(file)
+
+        bmp.compress(Bitmap.CompressFormat.PNG, 85, fOut)
+        fOut.flush()
+        fOut.close()
     }
 
     fun open_save_place(view: View) {
